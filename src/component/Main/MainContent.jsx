18601bde26575fc088ec7de7d475d9878962f6d7
Pinotmeme <caokyphong98@gmail.com>
2025-04-04 17:42:53 +0700

    Initial commit

---
+++ src/component/Main/MainContent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MainContent = () => {
+  return (
+    <main className="main">
+      <section className="hero section dark-background">
+
+      </section>
+    </main>
+  );
+};
+export default MainContent;
